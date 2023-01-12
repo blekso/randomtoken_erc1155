e5ea685a8640a4b04d672764b2c8af4804bb5028
blekso <blekepix@outlook.com>
2023-01-12 20:24:43 +0100

    changs on contract

--- contracts/RandomToken.sol
+++ contracts/RandomToken.sol
@@ -4,12 +4,19 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
 import "./interfaces/IRandomToken.sol";
 
 import "hardhat/console.sol";
 
-contract RandomToken is ERC1155, Ownable, IRandomToken, AccessControl {
+contract RandomToken is
+    ERC1155,
+    Ownable,
+    IRandomToken,
+    AccessControl,
+    ERC1155Holder
+{
     uint256 public price;
     uint256 public rewardAmount;
     uint256 public upgradePrice;
@@ -20,12 +27,9 @@ contract RandomToken is ERC1155, Ownable, IRandomToken, AccessControl {
 
     mapping(uint256 => address) public battleWinner;
     mapping(uint256 => Champion) public tokenIdToChampion;
-    mapping(address => uint256[]) public userOwnedChampions;
 
     constructor(
         string memory _uri,
-        string memory _name,
-        string memory _symbol,
         uint256 _price,
         uint32 _maxSupply,
         uint256 _rewardAmount
@@ -39,56 +43,54 @@ contract RandomToken is ERC1155, Ownable, IRandomToken, AccessControl {
         public
         view
         virtual
-        override(ERC1155, AccessControl)
+        override(ERC1155, AccessControl, ERC1155Receiver)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
     }
 
-    function mint() external payable {
+    function mint(uint256 id) external payable {
         if (msg.value < price) revert Underpriced();
         if (currentSupply + 1 > maxSupply) revert MaxSupplyReached();
 
         Champion memory newChampion = _createRandomChampion(++currentSupply);
         tokenIdToChampion[currentSupply] = newChampion;
 
-        _mint(msg.sender, currentSupply, 1, "");
-
-        userOwnedChampions[payable(msg.sender)].push(newChampion);
+        _mint(msg.sender, currentSupply, id, "");
     }
 
     function rewardPlayer(address player) internal {
         _mint(player, rewardAmount, 1, "");
     }
 
-    function attack(uint256 _myChampion, uint256 _enemyChampion) external {
+    function attack(uint256 _myChampionId, uint256 _enemyChampionId) external {
         if (
-            userOwnedChampions[_myChampion] != msg.sender &&
-            userOwnedChampions[_enemyChampion] == msg.sender
+            balanceOf(msg.sender, _myChampionId) != 0 &&
+            balanceOf(msg.sender, _enemyChampionId) == 1
         ) revert Unauthorized();
 
         if (
-            tokenIdToChampion[_enemyChampion].defensePower <
-            tokenIdToChampion[_myChampion].attackPower
+            tokenIdToChampion[_enemyChampionId].defensePower <
+            tokenIdToChampion[_myChampionId].attackPower
         ) {
             address attacker = msg.sender;
             battleWinner[++battleCount] = attacker;
 
             rewardPlayer(attacker);
-        } else {
-            address defender = owner(_enemyChampion);
+        } /* else {
+            address defender = owner(_enemyChampionId);
             battleWinner[++battleCount] = defender;
 
             rewardPlayer(defender);
-        }
+        } */
     }
 
     function upgradeChampion(
-        uint256 _myChampion,
+        uint256 _myChampionId,
         bool attackPowerIncrease,
         bool defensePowerIncrease
     ) external payable {
-        if (userOwnedChampions[_myChampion] == address(0))
+        if (balanceOf(address(0), _myChampionId) == 1)
             revert TokenDoesNotExist();
         if (!attackPowerIncrease && !defensePowerIncrease)
             revert NoStatsToIncrease();
@@ -101,14 +103,14 @@ contract RandomToken is ERC1155, Ownable, IRandomToken, AccessControl {
         }
 
         if (attackPowerIncrease) {
-            tokenIdToChampion[_myChampion].attackPower += 10;
+            tokenIdToChampion[_myChampionId].attackPower += 10;
         }
         if (defensePowerIncrease) {
-            tokenIdToChampion[_myChampion].defensePower += 10;
+            tokenIdToChampion[_myChampionId].defensePower += 10;
         }
 
         emit ChampionUpgraded(
-            _myChampion,
+            _myChampionId,
             attackPowerIncrease,
             defensePowerIncrease
         );
@@ -134,8 +136,6 @@ contract RandomToken is ERC1155, Ownable, IRandomToken, AccessControl {
         );
 
         return Champion({attackPower: randomAtk, defensePower: randomDef});
-
-        tokenIdToChampion[++currentSupply] = this.onERC1155Received.selector;
     }
 
     function onERC1155Received(
@@ -144,7 +144,7 @@ contract RandomToken is ERC1155, Ownable, IRandomToken, AccessControl {
         uint256,
         uint256,
         bytes memory
-    ) public returns (bytes4) {
+    ) public pure override returns (bytes4) {
         return this.onERC1155Received.selector;
     }
 
@@ -154,7 +154,28 @@ contract RandomToken is ERC1155, Ownable, IRandomToken, AccessControl {
         uint256[] memory,
         uint256[] memory,
         bytes memory
-    ) public returns (bytes4) {
+    ) public pure override returns (bytes4) {
         return this.onERC1155BatchReceived.selector;
+    }
+
+    function swapChampion(uint256 _myChampionId, uint256 _wantedChampionId)
+        external
+        payable
+    {
+        if (balanceOf(msg.sender, _myChampionId) == 1) revert Underpriced();
+
+        require(
+            balanceOf(address(this), _wantedChampionId) == 1,
+            "Sorry, wanted champion id is currently not in stock."
+        );
+
+        this.safeTransferFrom(msg.sender, address(this), _myChampionId, 1, "");
+        this.safeTransferFrom(
+            address(this),
+            msg.sender,
+            _wantedChampionId,
+            1,
+            ""
+        );
     }
 }
